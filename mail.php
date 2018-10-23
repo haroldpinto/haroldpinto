51f91e52d281053585eb76d41e4d94fedd48d0df
Harold <harpinmer@hotmail.com>
2018-10-22 19:08:07 -0500

    mail.php modification and iphone rendering changes

--- mail.php
+++ mail.php
@@ -1,38 +1,67 @@
 <?php
-
-if (isset($_POST['yourName'])) && 
-	isset($_POST['yourEmail']) && 
-	isset($_POST['yourSubject']) && 
-	isset($_POST['yourMessage'])) {
-	$yourName = $_POST['yourName'];
-	$yourEmail = $_POST['yourEmail'];
-	$yourSubject = $_POST['yourSubject'];
-	$yourMessage = $_POST['yourMessage'];
-	$to = 'harpinmer@hotmail.com';
-	$subject = "New Request - Portfolio";
-	$body = '<html>
-				<body>
-					<h2>Feedback - My Portfolio</h2>
-					<hr>
-					<p>Name:<br>'.$yourName.'</p>
-					<p>Email:<br>'.$yourEmail.'</p>
-					<p>Subject:<br>'.$yourSubject.'</p>
-					<p>Mesage:<br>'.$yourmessage.'</p>
-				</body>
-			</html>';
-
-	$headers = "From: ".$yourName." <".$yourEmail">\r\n";
-	$headers .= "Reply_To: ".$yourEmail."\r\n";
-	$headers .= "MIME-Version: 1.0\r\n";
-	$headers .= "Content-type: text/html: charset=utf-8";
-
-	$send = mail($to, $subject, $body, $headers);
-	if ($send) {
-		echo <'<br>';
-		echo 'Message successfully sent. I will be back with you shortly.';
-	else {
-		echo 'An error was encountered when trying to send your message. Please verify the information entered and try again.';
-	}
+if(isset($_POST['email'])) {
+ 
+    // EDIT THE 2 LINES BELOW AS REQUIRED
+    $email_to = "harpinmer@hotmail.com";
+    $email_subject = "Portfolio - Contact Request";
+ 
+    function died($error) {
+        // your error code can go here
+        echo "We are very sorry, but there were error(s) found with the form you submitted. ";
+        echo "These errors appear below.<br /><br />";
+        echo $error."<br /><br />";
+        echo "Please go back and fix these errors.<br /><br />";
+        die();
+    }
+ 
+ 
+    // validation expected data exists
+    if(!isset($_POST['yourName']) ||
+        !isset($_POST['yourEmail']) ||
+        !isset($_POST['yourSubject']) ||
+        !isset($_POST['yourMessage'])) {
+        died('We are sorry, but there appears to be a problem with the form you submitted.');       
+    }
+ 
+     
+ 
+    $your_name = $_POST['yourName']; // required
+    $your_email = $_POST['yourEmail']; // required
+    $your_subject = $_POST['yourSubject']; // not required
+    $your_message = $_POST['yourMessage']; // required
+ 
+    $error_message = "";
+    $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
+ 
+  if(!preg_match($email_exp,$your_email)) {
+    $error_message .= 'The Email Address you entered does not appear to be valid.<br />';
+  }
+ 
+    $email_message = "Form details below.\n\n";
+ 
+     
+    function clean_string($string) {
+      $bad = array("content-type","bcc:","to:","cc:","href");
+      return str_replace($bad,"",$string);
+    }
+ 
+    $email_message .= "Name: ".clean_string($your_name)."\n";
+    $email_message .= "Email: ".clean_string($your_email)."\n";
+    $email_message .= "Subject: ".clean_string($your_subject)."\n";
+    $email_message .= "Message: ".clean_string($your_message)."\n";
+ 
+// create email headers
+$headers = 'From: '.$your_email."\r\n".
+'Reply-To: '.$your_email."\r\n" .
+'X-Mailer: PHP/' . phpversion();
+@mail($email_to, $email_subject, $email_message, $headers);  
+?>
+ 
+<!-- include your own success html here -->
+ 
+Thank you for contacting us. We will be in touch with you very soon.
+ 
+<?php
+ 
 }
-
 ?>
